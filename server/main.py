c4f04bafb6c79eaf568d23fff7c1306bf8ea1fbc
Ryan Johnson <ryan.johnson.1315@gmail.com>
2020-11-12 22:56:14 -0700

    need to set up the career

--- server/main.py
+++ server/main.py
@@ -6,27 +6,27 @@ import nba_api.stats.static.players as players
 def build_routes(app):
     player = players.find_players_by_full_name('donovan mitchell')[0]
     print(player)
-    @app.route('/get', methods=['GET'])
+    @app.route('/career', methods=['GET'])
+    
     def home():
         data = pc.PlayerCareerStats(player['id']).get_dict()
         
         headers = []
         rowSets   = []
         for f in data['resultSets']:
-            print(f)
             for d in f:
                 headers = f['headers']
                 rowSets = f['rowSet']
 
-        print('\n\n\n')
-        goingBack = {}
-        for rowSet in rowSets:
-            for h, d in zip(headers, rowSet):
-                print(f'{h}: {d}')
-                goingBack[h] = d
-            print('\n\n\n')
+        # print('\n\n\n')
+        # goingBack = {}
+        # for rowSet in rowSets:
+        #     for h, d in zip(headers, rowSet):
+        #         print(f'{h}: {d}')
+        #         goingBack[h] = d
+        #     print('\n\n\n')
         
-        return {'data': goingBack}
+        return {'data': rowSets, 'keys': headers}
 
 if __name__ == "__main__":
     app = flask.Flask(__name__)
